424de03aa468bbbde580758b8383fc5251ddc29b
GabrielStahlberg <gabrielstahlberg@gmail.com>
2021-02-17 14:47:25 -0300

    Pageable refactor.

--- src/main/java/br/com/gstahlberg/spring_training/SpringTrainingApplication.java
+++ src/main/java/br/com/gstahlberg/spring_training/SpringTrainingApplication.java
@@ -2,8 +2,10 @@ package br.com.gstahlberg.spring_training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class SpringTrainingApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/gstahlberg/spring_training/controller/ProductController.java
+++ src/main/java/br/com/gstahlberg/spring_training/controller/ProductController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -26,10 +28,7 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity<Page<Product>> allProducts(@RequestParam(required = false) EnumCategory category,
-                                                     @RequestParam int page,
-                                                     @RequestParam int quantityPerPage) {
-
-        Pageable pagination = PageRequest.of(page, quantityPerPage);
+                                                     @PageableDefault(sort = "id", direction = Sort.Direction.ASC, page = 0, size = 5) Pageable pagination) {
 
         if(category != null) {
             return ResponseEntity.ok().body(service.findProductByCategory(pagination, category));
